14eef614b536b29e1be674ec4b50cdec427a15c7
sINFlumetz <lukas.metzner@stud.th-rosenheim.de>
2021-06-14 23:12:52 +0200

    added kuebiko class

---
+++ kuebiko/__init__.py
@@ -0,0 +1,1 @@
+from kuebiko.kuebiko import Kuebiko
---
+++ kuebiko/kuebiko.py
@@ -0,0 +1,6 @@
+class Kuebiko:
+    def __init__(self) -> None:
+        pass
+
+    def load_dataset(self, query_file: str) -> list:
+        return []
